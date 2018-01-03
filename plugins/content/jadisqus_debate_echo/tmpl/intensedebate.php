ed857eceae9e57917c1498592c5301d00336e611
PC-201207261050\Administrator <415081286@qq.com>
2018-01-03 11:07:44 +0800

    删除评论部分

--- plugins/content/jadisqus_debate_echo/tmpl/intensedebate.php
+++ plugins/content/jadisqus_debate_echo/tmpl/intensedebate.php
@@ -19,27 +19,3 @@ $account        = $this->plgParams->get('provider-intensdebate-account');
 $url = str_replace('&amp;', '&', $this->_url );
 ?>
 
-<?php if ($this->commentContext == 'count'): ?>
-	<span class="containerCountComment" onclick="window.location.href='<?php echo $this->_url;?>#idc-container'; return false;">
-	<script id="idebate-counter" type="text/javascript">
-	//<![CDATA[
-		idcomments_acct = "<?php echo $account?>";
-		idcomments_post_id = "<?php echo $this->_postid_debate?>";
-		idcomments_post_url = encodeURIComponent("<?php echo $url;?>");
-	//]]>
-	</script>
-	<input type="hidden" id="debate-counter"/>
-	<script type="text/javascript" src="http://www.intensedebate.com/js/genericLinkWrapperV2.js"></script>
-    </span>
-<?php else: ?>
-	<script id="idebate-comment" type="text/javascript">
-	//<![CDATA[
-		idcomments_acct = "<?php echo $account?>";
-		idcomments_post_id = "<?php echo $this->_postid_debate?>";
-		idcomments_post_url = "<?php echo $this->_url?>";
-	//]]>
-	</script>
-	<span id="IDCommentsPostTitle" style="display:none"></span>
-	<input type="hidden" id="debate-comment"/>
-	<script type="text/javascript" src="http://www.intensedebate.com/js/genericCommentWrapperV2.js"></script>
-<?php endif; ?>
